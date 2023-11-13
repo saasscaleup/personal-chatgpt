3f5a27841d56c32e27020e34b8f1bc00d4af963e
dani <dataextractai@gmail.com>
2023-11-13 18:09:41 +0200

    fastapi - added fastapi feature

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, HTTPException
+from GodChatGPT import GodChatGPT
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {"hello":"word"}
+
+@app.get("/god-chatgpt")
+def run_god_chatgpt(query: str):
+    god_chatgpt = GodChatGPT()
+    result = god_chatgpt.agent_executor({"input": query})
+    print(result)
+    return {"result":result['output']}
