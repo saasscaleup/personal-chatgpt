f8f0538d4519058be55e9c779d51fbe504305884
dani <dataextractai@gmail.com>
2023-11-15 19:44:09 +0200

    streamlit - final god chatgpt streamlit version

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,10 +1,12 @@
 import streamlit as st
 from GodChatGPT import GodChatGPT
 
-@st.cache_resource  # 👈 Add the caching decorator
+@st.cache_resource
 def load_god_chatgpt():
     god_chatgpt = GodChatGPT(st.secrets["openai_apikey"],st.secrets["serpapi_apikey"])
     return god_chatgpt
+
+god_chatgpt = load_god_chatgpt()
 
 if 'user_input' not in st.session_state:
     st.session_state.user_input = ''
@@ -13,20 +15,16 @@ def submit():
     st.session_state.user_input = st.session_state.query
     st.session_state.query = ''
 
-god_chatgpt = load_god_chatgpt();
+st.title("🔥 Welcome to GOD-ChatGPT 🔥")
 
-# Set page title
-st.title("🔥 Wellcome to GOD-ChatGPT 🔥")
-
-st.text_input("Play with me:",key='query', on_change=submit)
+st.text_input("Play with me:", key='query', on_change=submit)
 
 user_input = st.session_state.user_input
-# Display the user input
-st.write("You entered:", user_input)
 
-if user_input:
-    result = god_chatgpt.agent_executor({"input": user_input})
-    print(result)
+st.write("Your entred: ", user_input)
 
-    # Additional interaction with the input 
-    st.write("🔥 GOD-ChatGPT Answer: ", result['output'])
+result = god_chatgpt.agent_executor({"input":user_input})
+
+print(result)
+
+st.write("🔥 GOD-ChatGPT Answer: ", result["output"])
